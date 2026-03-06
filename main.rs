aeb3a78c5bdd14b5562f20bc61926d10dc86100c
DestinedMC <muhammedibnsahal@gmail.com>
2026-03-06 16:28:55 +0300

    Update main.rs
    Added code to run the terminal a

--- main.rs
+++ main.rs
@@ -1,1 +1,18 @@
+//welcome to the bluk of the code.
+//the OS is mainly written in rust
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+
+#[no_mangle]
+
+pub extern "C" fn _start -> ! {
+  loop{}
+}
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> !{
+  loop{}
+}
 
