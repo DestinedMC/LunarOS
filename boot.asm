25c2cf9f5ad6d990bd28b9c26880d31e2620fb13
DestinedMC <muhammedibnsahal@gmail.com>
2026-03-06 15:40:45 +0300

    Update boot.asm
    Added the code for the boot loader

--- boot.asm
+++ boot.asm
@@ -1,1 +1,34 @@
+;boot asm it's the boot loader of Lunar
+section .multiboot
+align 4
+  dd 0x1BADB002
+  dd 0x00
+  dd -(0x1BADB002 + 0x00)
+
+section .bss
+align 16
+
+stack_bottom:
+ resb 16384
+ stack_top:
+
+section .text
+ global start
+ extern _start
+
+start:
+ mov esp, stack_top
+ call _start
+ cli
+
+.hang:
+ hlt
+ jmp .hang
+
+
+
+
+
+
+
 
